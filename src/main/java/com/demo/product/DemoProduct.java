cae76f0b41c09686bf47c5bbae07470e4c001a49
David7557 <davidkumar7557@gmail.com>
2025-02-15 20:16:25 +0530

    Creating Product entity with it's fields

---
+++ src/main/java/com/demo/product/DemoProduct.java
@@ -0,0 +1,33 @@
+package com.demo.product;
+
+public class DemoProduct {
+    private int id;
+    private String productname;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProductname() {
+        return productname;
+    }
+
+    public void setProductname(String productname) {
+        this.productname = productname;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    private  int price;
+
+}
